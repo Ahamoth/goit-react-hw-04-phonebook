ad9c0150f5b71716ce995c89f3e0245a26f46c5b
Ahamoth <beeminer.store@gmail.com>
2023-03-11 05:25:30 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
+import { ContactForm } from './Form/Form';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useState, useEffect } from 'react';
+
+const LS_KEY = 'contactbook-items';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem(LS_KEY)) ?? contacts
+  );
+  const [filter, setFilter] = useState('');
+
+  const addContact = newContact => {
+    if (contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return '';
+    }
+    setContacts(prevContacts => [newContact, ...prevContacts]);
+  };
+
+  const handleFilter = text => {
+    setFilter(text);
+  };
+
+  const onDeleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="wrapper">
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter onChange={handleFilter} value={filter} />
+      <ContactList
+        contacts={contacts}
+        filter={filter}
+        onDeleteContact={onDeleteContact}
+      />
     </div>
   );
 };
